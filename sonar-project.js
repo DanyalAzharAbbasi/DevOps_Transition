eaecd9697c44854984abc757806b38e6840b564e
DanyalAzharAbbasi <123732947+DanyalAzharAbbasi@users.noreply.github.com>
2023-06-20 18:44:34 +0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,6 +4,8 @@ scanner(
   {
     serverUrl : 'http://10.0.3.46:9000',
     token : "sqp_6d5e7eacff2c1c532d31c4007a988f970c6add7b",
+    login: "admin",
+    password: "12345",
     options: {
       'sonar.projectName': 'Transition'
     }
