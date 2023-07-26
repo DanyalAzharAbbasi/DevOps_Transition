d9ddd1ecf4a984814c4aa035bacbabea5f485de3
DanyalAzharAbbasi <123732947+DanyalAzharAbbasi@users.noreply.github.com>
2023-07-27 00:18:55 +0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,7 +3,7 @@ const scanner = require('sonarqube-scanner');
 scanner(
   {
     serverUrl : 'http://192.168.10.4:9000',
-    token : "sqp_6d5e7eacff2c1c532d31c4007a988f970c6add7b",
+    token : "sqp_b7f39fab972756bb6b868c992fc578fcb1487f3f",
     options: {
       'sonar.projectName': 'Transition'
     }
